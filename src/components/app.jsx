b322514539851a206c7e31b4c7fa21be1c0746b4
Squibs <z.r.holman+github@gmail.com>
2018-04-03 22:51:35 -0600

    'src' folder structure. Added react, react-dom, react-redux, and redux.

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>React simple starter</div>
+    );
+  }
+}
